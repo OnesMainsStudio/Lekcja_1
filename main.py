fadd9491005bd353f82e0bec1e5def69109a3c99
OnesMainsStudio <145258515+OnesMainsStudio@users.noreply.github.com>
2023-09-17 11:58:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-import randrom as r
+meme_dict = {
+            "CRINGE": "coś dziwnego lub wstydliwego",
+            "LOL": "odpowiedź na coś zabawnego",
+            "ROFL": "odpowiedź na żart",
+            "SHEESH": "lekka dezaprobata",
+            "CREEPY": "straszny, złowieszczy",
+            "AGGRO": "stać się agresywnym/zły",
+            }
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
 
-run = True
-while run:
-  pass
+def checking():
+    global word, run
+    if word in meme_dict.keys():
+        print(word,":", meme_dict[word])
+        run = False
+    else:
+        print("there is no word like that in ")
+        
+
+checking()
